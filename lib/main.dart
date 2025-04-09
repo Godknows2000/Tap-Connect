69acb033601394ba45135e4702f907272d5466d9
Godknows2000 <92857706+Godknows2000@users.noreply.github.com>
2025-04-09 20:57:58 +0200

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -22,7 +22,7 @@ class MyApp extends StatelessWidget {
           ),
         ),
       ),
-      home: const SplashScreen(), // Start with splash screen
+      home: const SplashScreen(),
       debugShowCheckedModeBanner: false,
     );
   }
